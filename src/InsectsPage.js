034e0eb61cd183805c76d3bb88c500e097ca1521
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-23 10:25:11 -0700

    InsectsPage working

--- src/InsectsPage.js
+++ src/InsectsPage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getInsects } from './services/fetch-utils';
+import InsectsList from './InsectsList';
 
-export default function InsectsPage() {
+function App() {
+  const [insects, setInsects] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 40;
+
+  useEffect(() => {
+    async function fetch() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const insectsAC = await getInsects(from, to);
+
+      setInsects(insectsAC);
+    }
+    fetch();
+  }, [page]);
+
   return (
-    <div>This is the InsectsPage page</div>
+    <>
+      <h2>Current Page {page}</h2>
+      <div className='buttons'>
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
+        <button onClick={() => setPage(page + 1)}>Next Page</button>
+      </div>
+      <InsectsList insects={insects} />
+    </>
   );
 }
+export default App;
