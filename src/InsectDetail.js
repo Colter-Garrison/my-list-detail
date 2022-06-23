b933e8b50ef10c478cd92ef4bff71e8e9d7ccc3b
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-23 13:10:11 -0700

    lunch

--- src/InsectDetail.js
+++ src/InsectDetail.js
@@ -25,6 +25,26 @@ export default function InsectDetail() {
         <div className='insect-data'>
           <p>{insect.Name}</p>
           <img className='insect-img' src={insect.image} />
+          <p>#{insect.number}</p>
+          <p>Sell Price: {insect.Sell}</p>
+          <p>Where to Find: {insect.Where}</p>
+          <p>Weather: {insect.Weather}</p>
+          <p>Total Catches to Unlock: {insect.Total}</p>
+          <p>Catch Phrase: {insect.Phrase}</p>
+          <p>Description: {insect.Description}</p>
+          <h4>Appears:</h4>
+          <p>Jan: {insect.Jan}</p>
+          <p>Feb: {insect.Feb}</p>
+          <p>Mar: {insect.Mar}</p>
+          <p>Apr: {insect.Apr}</p>
+          <p>May: {insect.May}</p>
+          <p>Jun: {insect.Jun}</p>
+          <p>Jul: {insect.Jul}</p>
+          <p>Aug: {insect.Aug}</p>
+          <p>Sep: {insect.Sep}</p>
+          <p>Oct: {insect.Oct}</p>
+          <p>Nov: {insect.Nov}</p>
+          <p>Dec: {insect.Dec}</p>
         </div>
       </div>
     </>
